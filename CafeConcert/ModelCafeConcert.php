654e63222d98c16c573d2d6114d41c51f7475864
Philippe <philippe.viguier@afpa.fr>
2021-05-27 12:01:17 +0200

    concert v1

--- CafeConcert/ModelCafeConcert.php
+++ CafeConcert/ModelCafeConcert.php
@@ -4,7 +4,7 @@ class ModelCafeConcert{
 
     }
     public function getDataCafeConcert(){
-        $url = "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=cafes-concerts&q=";
+        $url = "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=cafes-concerts&q=&rows=26";
         $json = file_get_contents($url);
         $data = json_decode($json, true, 512, JSON_OBJECT_AS_ARRAY);
         return $data;
